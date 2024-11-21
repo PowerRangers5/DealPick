aff309c8875139ada3e0218665210201de93f81f
JinseongMOK <mjs4027@naver.com>
2024-11-21 23:44:54 +0900

    style: 스플래시 화면 완성

--- lib/screen/splash.dart
+++ lib/screen/splash.dart
@@ -1,13 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Splash extends StatelessWidget {
   const Splash({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Container(
-        decoration: BoxDecoration(color: Colors.amber),
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color(0xff4CB0C0), Color(0xff84DCA3)],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'DealPick',
+              style: GoogleFonts.lobster(
+                fontSize: 54,
+                fontWeight: FontWeight.normal,
+                color: Colors.white,
+              ),
+            ),
+            Text(
+              '가치있는 거래의 시작',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
       ),
     );
   }
