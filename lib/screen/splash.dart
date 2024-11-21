f1e8722b24b504fa9e454539346348f3d9852214
JinseongMOK <mjs4027@naver.com>
2024-11-22 00:00:05 +0900

    build: Scaffold 위젯 다시 splash.dart로

--- lib/screen/splash.dart
+++ lib/screen/splash.dart
@@ -6,42 +6,38 @@ class Splash extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Scaffold(
-          body: Container(
-            width: double.infinity,
-            height: double.infinity,
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                colors: [Color(0xff4CB0C0), Color(0xff84DCA3)],
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-              ),
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'DealPick',
-                  style: GoogleFonts.lobster(
-                    fontSize: 54,
-                    fontWeight: FontWeight.normal,
-                    color: Colors.white,
-                  ),
-                ),
-                Text(
-                  '가치있는 거래의 시작',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold),
-                ),
-              ],
-            ),
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color(0xff4CB0C0), Color(0xff84DCA3)],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
           ),
         ),
-      ],
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'DealPick',
+              style: GoogleFonts.lobster(
+                fontSize: 54,
+                fontWeight: FontWeight.normal,
+                color: Colors.white,
+              ),
+            ),
+            Text(
+              '가치있는 거래의 시작',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
