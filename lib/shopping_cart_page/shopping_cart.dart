262d1e4bacd7efeb1a022578b21e3b43e92fbe23
Min-woos <wcn67888@gmail.com>
2024-11-26 17:04:40 +0900

    main delets

--- lib/shopping_cart_page/shopping_cart.dart
+++ lib/shopping_cart_page/shopping_cart.dart
@@ -1,21 +1,5 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: ShoppingCartPage(),
-    );
-  }
-}
-
 class ShoppingCartPage extends StatefulWidget {
   @override
   _ShoppingCartPageState createState() => _ShoppingCartPageState();
